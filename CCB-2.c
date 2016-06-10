f284edbb8376469a2358d78c4c220c804359a5df
kapilagrawal <kapil.agrawal81@gmail.com>
2016-06-10 10:10:56 +0530

    Update CCB-2.c

--- CCB-2.c
+++ CCB-2.c
@@ -6,21 +6,23 @@ int main(void)
 	int amt;
 	float bal;
 	scanf("%d" "%f", &amt, &bal);
-	
-	if((amt % 5) != 0)
+	if((amt > 0 && amt <= 2000) && (bal >= 0 && bal <= 2000))
 	{
-	    printf("%.2f\n", bal);
-	    return 0;
-	}
-	else if(amt > (int)bal)
-	{
-	    printf("%.2f\n", bal);
-	    return 0;
-	}
-	else
-	{
-	    bal = bal - (float)amt - 0.5;
-	    printf("%.2f\n", bal);
+	   if((amt % 5) != 0)
+	   {
+	   	   printf("%.2f\n", bal);
+	       return 0;
+	   }
+	   else if( (bal < ( (float)amt+0.50) ) )
+	   {
+	       printf("%.2f\n", bal);
+	       return 0;
+	   }
+	   else
+	   {
+	       bal = bal - (float)amt - 0.5;
+	       printf("%.2f\n", bal);
+	   }
 	}
 	return 0;
 }
