38f823f24ea071bf069ff3f39e2bebc9d4ae2ed1
kapilagrawal <kapil.agrawal81@gmail.com>
2016-06-11 15:01:11 +0530

    CCB-3.c
    Enormous Input Test

---
+++ CCB-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int i, n, k, x;
+	int count = 0;
+	scanf("%d %d", &n, &k);
+	for(i = 0;i < n; i++)
+	{
+	    scanf("%d", &x);
+	    if(x % k == 0)
+	        count++;
+	}
+	printf("%d", count);
+	return 0;
+}
