c5daea76241aa129e5ee358f90c06cd6de7857fd
kapilagrawal <kapil.agrawal81@gmail.com>
2016-06-14 15:49:11 +0530

    CCB-4.c
    Factorial Zeros

---
+++ CCB-4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int power(int x, int y)
+{
+    int i, r = 1;
+    for (i = 1; i <= y; i++)
+    {
+        r = r * x;
+    }
+    return r;
+}
+
+int main()
+{
+    int r, d, l, j, i = 1, cnt = 0;
+    scanf("%d", &l);
+    int n[l], z[l];
+    for(j=0;j<l;j++)
+    {
+        scanf("%d", &n[j]);
+    }
+    for(j=0;j<l;j++)
+    {
+        i = 1;
+        cnt = 0;
+        r = power(5, i);
+        while(r <= n[j])
+        {
+            d = n[j]/r;
+            cnt = cnt + d;
+            i++;
+            r = power(5, i);
+        }
+        z[j] = cnt;
+    }
+    for(j=0;j<l;j++)
+    {
+        printf("%d\n", z[j]);
+    }
+    return 0;
+}
